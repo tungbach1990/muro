f4d8bf7a38519ff7ffe3aec4d5287643071b2164
Mr. Jor <joroluck@yahoo.com>
2024-02-15 11:27:11 +0700

    fix build

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -868,7 +868,7 @@
 	export_constant2("bMaxPenRate",SP_MAX_PEN_RATE);
 	export_constant2("bMaxDamageExceed",SP_MAX_DAMAGE_EXCEED);
 	export_constant2("bMaxDamagePenExceed",SP_MAX_DAMAGE_PEN_EXCEED);
-	export_constant2("bAddReborn",SP_ADDREBORN);
+	export_constant2("bAddReborn",SP_REBORN);
 
 	/* equip indices */
 	export_constant(EQI_COMPOUND_ON);
